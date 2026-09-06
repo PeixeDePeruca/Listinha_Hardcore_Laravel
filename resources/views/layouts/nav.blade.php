3c75c883456e15f4dafe93b6cf4eae6f3065cbdd
Anchova <anchovaluiz@gmail.com>
2026-09-06 20:21:16 -0300

    Tema 4 - Desafio

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/alunos-crud">Lista de Alunos</a></li>
+        <li><a href="/alunos-crud/create">Cadastrar Aluno</a></li>
+    </ul>
+</nav>
