3781a8707ffaeb2b6ecd9d1e080725314cd1384f
Anchova <anchovaluiz@gmail.com>
2026-09-06 17:10:31 -0300

    Tema 1 - Atividade 2
    TEMA 1: Rotas

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
 });
 
 
-// ATV 1: Rotas estáticas
+// Atividade 1: Rotas estáticas
 Route::get('/sobre', function () {
     return '- Página Sobre -';
 });
@@ -18,4 +18,17 @@ Route::get('/alunos', function () {
 
 Route::get('/contato', function () {
     return '- Página de Contato -';
+});
+
+//Atividade 2: Rotas com parâmetros
+Route::get('/produto/{id}', function ($id) {
+    return "Exibindo o produto {$id}";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Exibindo a categoria {$id}";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Exibindo o usuário {$id}";
 });
