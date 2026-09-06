90b99e3e18b904059a6fcbbe3b12530826891f6e
Anchova <anchovaluiz@gmail.com>
2026-09-06 20:03:54 -0300

    Tema 4 - Atividade 9

--- resources/views/alunos/index.blade.php
+++ resources/views/alunos/index.blade.php
@@ -1,1 +1,19 @@
-<h1>- Lista de Alunos -</h1>
+@extends('layouts.app')
+
+@section('content')
+    <h1>- Lista de Alunos -</h1>
+
+    @php 
+        $alunos = ['João Silva', 'Maria Santos', 'Pedro Oliveira']; 
+    @endphp
+
+    @if(empty($alunos))
+        <p>Nenhum aluno cadastrado no momento.</p>
+    @else
+        <ul>
+            @foreach($alunos as $aluno)
+                <li>{{ $aluno }}</li>
+            @endforeach
+        </ul>
+    @endif
+@endsection
