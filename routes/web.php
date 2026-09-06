f434dca08a8b21081d94fe98b457540a8a9c7650
Anchova <anchovaluiz@gmail.com>
2026-09-06 15:38:34 -0300

    Atividade 1 (Criar rotas)
    Commit da atv 1
    
    Objetivo: criar as routes: '/sobre', '/alunos' e '/contato', que devem apenas retornar um texto

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// ATV 1: Rotas estáticas
+Route::get('/sobre', function () {
+    return '- Página Sobre -';
+});
+
+Route::get('/alunos', function () {
+    return '- Lista de Alunos -';
+});
+
+Route::get('/contato', function () {
+    return '- Página de Contato -';
+});
